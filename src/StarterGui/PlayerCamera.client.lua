8bf71021c88dbb41ee2e438ee7006a0d001f91c7
Allan Ing <xeero@yahoo.com>
2021-05-31 22:04:04 -0500

    Three level zoom via Space key

--- src/StarterGui/PlayerCamera.client.lua
+++ src/StarterGui/PlayerCamera.client.lua
@@ -5,9 +5,11 @@
 local Workspace = game:GetService("Workspace")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local RunService = game:GetService("RunService")
-local Players = game:GetService("Players")
-
 local Settings = require(ReplicatedStorage.Settings)
+
+local ContextActionService = game:GetService("ContextActionService")
+
+local Players = game:GetService("Players")
 
 
 -- Variables for the camera and player
@@ -16,14 +18,15 @@ local Player = Players.LocalPlayer
 
 local character = Player.Character or Player.CharacterAdded:wait()
 
+-- Constant variable used to set the camera's offset from the player
+local X_ANGLE = 0
+local Y_HEIGHT_LIST = { 12, 20, 32 }
+local Z_ANGLE_LIST = { 12, 16, 18 }
+
+local currentIdx = 2  -- Start at medium height
+local currentCameraOffset = Vector3.new(X_ANGLE, Y_HEIGHT_LIST[currentIdx], Z_ANGLE_LIST[currentIdx])
 
 if Settings.IsFixedCameraAngle then
-  -- Constant variable used to set the camera's offset from the player
-  local X_ANGLE = 0
-  local Y_HEIGHT = 20
-  local Z_ANGLE = 16
-  local CAMERA_OFFSET = Vector3.new(X_ANGLE, Y_HEIGHT, Z_ANGLE)
-
   -- Enables the camera to do what this script says
   camera.CameraType = Enum.CameraType.Scriptable
 
@@ -32,7 +35,8 @@ if Settings.IsFixedCameraAngle then
     -- Check if the player's character has spawned
     if character then
       local playerPosition = character.HumanoidRootPart.Position
-      local cameraPosition = playerPosition + CAMERA_OFFSET
+      currentCameraOffset = Vector3.new(X_ANGLE, Y_HEIGHT_LIST[currentIdx], Z_ANGLE_LIST[currentIdx])
+      local cameraPosition = playerPosition + currentCameraOffset
 
       -- make the camera follow the player
       camera.CoordinateFrame = CFrame.new(cameraPosition, playerPosition)
@@ -40,4 +44,23 @@ if Settings.IsFixedCameraAngle then
   end
   RunService:BindToRenderStep("Camera", Enum.RenderPriority.Camera.Value, onRenderStep)
 end
+
+
+-- Setting up the action handling function
+local function handleAction(actionName, inputState, inputObj)
+  if inputState == Enum.UserInputState.Begin then
+    if currentIdx == 3 then
+      currentIdx = 1
+    else
+      currentIdx += 1
+    end
+  end
+  -- Since this function does not return anything, this handler will
+  -- "sink" the input and no other action handlers will be called after
+  -- this one.
+end
+
+-- Bind the action to the handler
+-- Ref: https://developer.roblox.com/en-us/api-reference/function/ContextActionService/BindAction
+ContextActionService:BindAction("BoundAction", handleAction, false, Enum.KeyCode.Space)
 
